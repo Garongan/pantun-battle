55807c6ce3f623821cff96ebde3be1f5f8291a7c
Garongan <alvindotri@student.ub.ac.id>
2023-03-08 21:25:44 +0700

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ShareIG from './pages/ShareIG';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/MyPersonalData' element={<MyPersonalData />} />
