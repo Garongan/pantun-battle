54ca844d1c77e710e0a6bd721de16b7a2191dd35
Garongan <alvindotri@student.ub.ac.id>
2023-03-09 10:37:15 +0700

    update 9 maret

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import HomeTitle from "../components/HomeTitle"
 import Banner from "../components/Banner"
 import Button from "../components/Button"
 
@@ -7,9 +6,9 @@ const Home = () => {
     return (
         <div className="container center">
             <div className="row row-cols-1">
-                <HomeTitle />
+                {/* Home Banner */}
                 <Banner />
-                <div className="col d-flex justify-content-center my-4">
+                <div className="col d-flex justify-content-center my-5">
                     <Button name="JOIN NOW" link="/MyPersonalData" type="button" />
                 </div>
             </div>
