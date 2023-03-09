b1434df937a0e0702bfaf581120303ac13bbd855
Garongan <alvindotri@student.ub.ac.id>
2023-03-09 17:59:19 +0700

    update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,7 @@
 function Banner () {
     return (
             <div class="col mt-4">
-                <img src={process.env.PUBLIC_URL + '/img/LOGO PANTUN BATTLE.png'} className="d-block banner w-100 mx-auto rounded" alt="banner" />
+                <img src={process.env.PUBLIC_URL + '/img/LOGO PANTUN BATTLE.png'} className="d-block w-100 mx-auto rounded" alt="banner" />
             </div>
     )
 }
